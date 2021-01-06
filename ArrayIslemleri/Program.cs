daaa91a6bf75e1e2b63cd48b281c0dccd5f9aa1e
İrem Aras <irem.aras@outlook.com>
2021-01-06 23:15:33 +0300

    deneme

--- ArrayIslemleri/Program.cs
+++ ArrayIslemleri/Program.cs
@@ -10,7 +10,26 @@ namespace ArrayIslemleri
     {
         static void Main(string[] args)
         {
+            int[] theArray = { 32, 79, 68, 97, 60, 98, 3 };
+            int max = 0;
+            int min = 100;
+            int toplam = 0;
+            int ortalama = 0;
+            for (int i = 0; i < theArray.Length; i++)
+            {
+                if (theArray[i] > max)
+                {
+                    max = theArray[i];
+                }
+                if (theArray[i] < min)
+                {
+                    min = theArray[i];
+                }
 
+                toplam += theArray[i];
+            }
+
+            ortalama = toplam / theArray.Length;
         }
     }
 }
